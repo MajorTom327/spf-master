70a723d3bc2d2088dc7fbb915dd1222ae01d9b59
vthomas <me@valentin-thomas.com>
2020-06-17 17:56:06 +0200

    Trying to add types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { Inspector } from './SpfInspect';
 
 export { Inspector as SpfInspector } from './SpfInspect';
+
+export { Record, SpfMechanism, SpfRecord, SpfType } from './types/Record';
+export { InspecterError, InspecterOptions, InspecterResults, InspecterSearch } from './types/Inspecter';
